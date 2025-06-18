e25a239fa57f60aa541426578aa001eef30c7bd3
ouadie-pro <ahmedfantouz@gmail.com>
2025-06-18 17:03:52 +0100

    third change

--- interface.js
+++ interface.js
@@ -8,6 +8,10 @@ let manBtn = document.getElementById('man');
 let womanBtn = document.getElementById('woman');
 let manCards = document.getElementById('man-cards');
 let womanCards = document.getElementById('woman-cards');
+let kidsbtn = document.getElementById('kids');
+let kidscard = document.getElementById('kids-cards')
+let shoesbtn = document.getElementById('shoes');
+let shoescard = document.getElementById('shoes-cards')
 
 womanBtn.addEventListener('click', function() {
     manCards.classList.add('hidden');
@@ -18,3 +22,15 @@ manBtn.addEventListener('click', function() {
     womanCards.classList.add('hidden');
     manCards.classList.remove('hidden');
 });
+
+kidsbtn.addEventListener('click',function(){
+    manCards.classList.add('hidden');
+    womanCards.classList.add('hidden');
+    kidscard.classList.remove('hidden');
+})
+shoesbtn.addEventListener('click',function(){
+    manCards.classList.add('hidden');
+    womanCards.classList.add('hidden');
+    kidscard.classList.add('hidden');
+    shoescard.classList.remove('hidden');
+})
