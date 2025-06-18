c9e3b50da34db56fe7af1bdc7c77312dab1736ba
ouadie-pro <ahmedfantouz@gmail.com>
2025-06-18 21:59:55 +0100

    third change 2

--- interface.js
+++ interface.js
@@ -5,7 +5,9 @@ menuIcon.addEventListener('click', function() {
 });
 
 let manBtn = document.getElementById('man');
+let manchoose = document.getElementById('man-choose');
 let womanBtn = document.getElementById('woman');
+let womanchoose = document.getElementById('woman-choose'); 
 let manCards = document.getElementById('man-cards');
 let womanCards = document.getElementById('woman-cards');
 let kidsbtn = document.getElementById('kids');
@@ -15,22 +17,33 @@ let shoescard = document.getElementById('shoes-cards')
 
 womanBtn.addEventListener('click', function() {
     manCards.classList.add('hidden');
+    manchoose.classList.add('hidden');
+    kidscard.classList.add('hidden');
+    shoescard.classList.add('hidden');
+    womanchoose.classList.remove('hidden');
     womanCards.classList.remove('hidden');
 });
 
 manBtn.addEventListener('click', function() {
     womanCards.classList.add('hidden');
+    manchoose.classList.remove('hidden');
+    womanchoose.classList.add('hidden');
     manCards.classList.remove('hidden');
 });
 
 kidsbtn.addEventListener('click',function(){
     manCards.classList.add('hidden');
     womanCards.classList.add('hidden');
+    shoescard.classList.add('hidden');
+    manchoose.classList.add('hidden');
+    womanchoose.classList.add('hidden');
     kidscard.classList.remove('hidden');
 })
 shoesbtn.addEventListener('click',function(){
     manCards.classList.add('hidden');
     womanCards.classList.add('hidden');
     kidscard.classList.add('hidden');
+    manchoose.classList.add('hidden');
+    womanchoose.classList.add('hidden');
     shoescard.classList.remove('hidden');
 })
